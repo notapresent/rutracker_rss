f89f92dd902328819eceb3a912922230fc862576
notapresent <notapresent@gmail.com>
2016-03-18 09:55:04 +0000

    Hook in bootstrap-treeview

--- static/index.js
+++ static/index.js
@@ -1,1 +1,8 @@
-//
+$(function(){
+    $.getJSON('https://storage.googleapis.com/rutracker-rss.appspot.com/category_map.json', {}, function(data, textStatus){
+
+        var tree = $('#ctree').treeview({
+          data: data
+        });
+    });
+});
