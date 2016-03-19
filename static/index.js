93f5f27fd4c56ffc69be517f0fed021e9fcaefd8
notapresent <notapresent@gmail.com>
2016-03-19 13:21:02 +0000

    Set correct JSON for production

--- static/index.js
+++ static/index.js
@@ -1,6 +1,5 @@
 $(function(){
     var url = 'https://storage.googleapis.com/rutracker-rss.appspot.com/category_map.json';
-    var url = 'https://dl.dropboxusercontent.com/u/660127/category_map.json';    // TODO
 
     $.getJSON(url, {}, function(data, textStatus){
 
