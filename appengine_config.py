8d6c0dc0ce478d78c77798a7c9484f90dfa2f4d9
notapresent <notapresent@gmail.com>
2016-03-24 23:17:11 +0000

    Appstats settings

--- appengine_config.py
+++ appengine_config.py
@@ -7,3 +7,7 @@ def webapp_add_wsgi_middleware(app):
   from google.appengine.ext.appstats import recording
   app = recording.appstats_wsgi_middleware(app)
   return app
+
+appstats_DATASTORE_DETAILS = True
+appstats_MAX_STACK = 20
+appstats_MAX_REPR = 250
